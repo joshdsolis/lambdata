d6f22d3d075b6b6dfa5bf1a13612faf1ccdd7be0
Josh Solis <joshsolis@joshs-mbp.attlocal.net>
2019-02-11 16:16:45 -0600

    Stub initial package, install numpy/pandas dependencies

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/USR/BIN/ENV PYTHON
+""" Package setup/installation and emtadata for lambdata
+"""
+
+import setuptools
+
+REQUIRED = [
+	"numpy",
+	"pandas",
+]
+
+with open("README.md", "r") as fh:
+	LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+	name = "lambdata-joshdsolis",
+	version= "0.0.1",
+	author ="joshdsolis",
+	description="A collection of Data Science helper functions",
+	long_description = LONG_DESCRIPTION,
+	long_description_content_type = "text/markdown",
+	url = "https://github.com/joshdsolis/lambdata",
+	packages=setuptools.find_packages(),
+	python_requires = ">=3.5",
+	install_requires=REQUIRED,
+	classifiers=[
+		"programming Language :: Python ::3"
+		"License :: OSI Approved :: MIT License",
+      	  	"Operating System :: OS Independent",
+    ]
+)
