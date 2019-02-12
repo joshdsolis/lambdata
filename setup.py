5255c1f3264dbeb1419909d8767dd20fb5da4580
Josh Solis <joshsolis@joshs-mbp.attlocal.net>
2019-02-12 14:41:14 -0600

    changing version of setup

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name = "lambdata-joshdsolis",
-	version= "0.0.6",
+	version= "0.0.7",
 	author ="joshdsolis",
 	description="A collection of Data Science helper functions",
 	long_description = LONG_DESCRIPTION,
